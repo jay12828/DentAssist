26a92b6e5f55287a6f992c2171d8a7087fb4dbe7
Jaydeep Chakrabarty <jaydeep.c1@gmail.com>
2011-05-16 23:52:54 +0530

    Login functionality

--- WebContent/Login.jsp
+++ WebContent/Login.jsp
@@ -3,14 +3,76 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<style type="text/css">
+body {
+	background-image: url(/DentAssist/backgrounds/green.jpg);
+	background-repeat: repeat;
+	color: #FFFFFF;
+	font-size:medium;
+}
+.btnLogin {
+	font-family: Verdana, Geneva, sans-serif;
+	color: #FFF;
+	background-color: #000000;
+}
+.login_table_inner {
+
+}
+.login_table_outer {
+	background-color: #000;
+}
+</style>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Login - DentAssist</title>
 </head>
 <body>
-<form method="post" action="login.do">
-Username: <input type="text" name="uname">
-Password: <input type="password" name="pword">
-<input type="submit" name="btnLogin" value="Login">
+<form id="login" name="login" method="post" action="login.do">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <table width="348" border="0" class="login_table_inner" align="right">
+  <tr>
+    <td width="72" height="27"></td>
+    <td width="84">&nbsp;</td>
+    <td width="168">&nbsp;</td>
+    <td width="72"></td>
+  </tr>
+  <tr>
+    <td height="49">&nbsp;</td>
+    <td>Username:</td>
+    <td><input type="text" name="uname" id="uname" /></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="34">&nbsp;</td>
+    <td>Password:</td>
+    <td><input type="password" name="pword" id="pword" /></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="btnLogin" class="btnLogin" value="Submit" /></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
 </form>
 </body>
 </html>
--- src/Servlets/Login/Login.java
+++ src/Servlets/Login/Login.java
@@ -1,6 +1,12 @@
 package Servlets.Login;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,12 +25,78 @@ import org.apache.catalina.Session;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     HttpSession session;
+    private static String dbURL = "jdbc:derby://localhost:1527/DentAssist;create=true";
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    public static String UserName;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Login() {
         super();
         // TODO Auto-generated constructor stub
+    }
+    private static void createConnection()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL); 
+        }
+        catch (Exception except)
+        {
+            except.printStackTrace();
+        }
+    }
+    
+    private static boolean authenticateUser(String username, String password)
+    {
+    	boolean user_validate = false;
+        try
+        {
+            stmt = conn.createStatement();
+            ResultSet results = stmt.executeQuery("select username, password from mine.REGISTRATION");
+
+            while(results.next())
+            {
+                String u_name = results.getString(1);
+                String p_word = results.getString(2);
+                if (username.equals(u_name)){
+                	if (password.equals(p_word)){
+                		user_validate = true;
+                	}
+                }
+            }
+            results.close();
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+		return user_validate;
+    }
+    
+    private static void shutdown()
+    {
+        try
+        {
+            if (stmt != null)
+            {
+                stmt.close();
+            }
+            if (conn != null)
+            {
+                DriverManager.getConnection(dbURL + ";shutdown=true");
+                conn.close();
+            }           
+        }
+        catch (SQLException sqlExcept)
+        {
+            
+        }
+
     }
 
 	/**
@@ -38,11 +110,19 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String user_name = request.getParameter("uname");
-		session = request.getSession(true);
-		session.setAttribute("username", user_name);
-		response.sendRedirect("Home.jsp");
+		String pass_word = request.getParameter("pword");
+
+		createConnection();
+		if (authenticateUser(user_name, pass_word)){
+			response.sendRedirect("Home.jsp");
+		}
+		else{
+			response.sendRedirect("error.jsp");
+		}
+		shutdown();
+		
+		
 	}
 
 }
